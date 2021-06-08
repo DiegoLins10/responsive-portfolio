2eae15cd674097c6cafc2be5743c220f2becd591
Diego Lins <68483773+DiegoLins10@users.noreply.github.com>
2021-06-07 23:49:27 -0300

    qualification att

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,7 +50,7 @@ skillsHeader.forEach((el) => {
 
 /*==================== QUALIFICATION TABS ====================*/
 const tabs = document.querySelectorAll('[data-target]'),
-      tabContents = document.querySelectorAll('[data-content]')
+tabContents = document.querySelectorAll('[data-content]')
 
 tabs.forEach(tab =>{
     tab.addEventListener('click', () =>{
@@ -61,7 +61,7 @@ tabs.forEach(tab =>{
         })
         target.classList.add('qualification__active')
 
-        tab.forEach(tab =>{
+        tabs.forEach(tab =>{
             tab.classList.remove('qualification__active')
         })
         tab.classList.add('qualification__active')
